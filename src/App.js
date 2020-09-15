70a5499ae047ef45da94cab85b07fc0229f2f273
Othior <splingaire.marvyn@gmail.com>
2020-09-15 10:22:36 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Board from './components/Board';
+import Card from './components/Card';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Board id="board-1" className="board">
+        <Card id="card-1" className="card" draggable="true">
+          <p>Card One</p>
+        </Card>
+      </Board>
+     <hr></hr>
+      <Board id="board-2" className="board">
+        <Card id="card-2" className="card" draggable="true">
+          <p>Card Two</p>
+        </Card>
+      </Board>
     </div>
   );
 }
